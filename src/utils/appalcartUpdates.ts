7bf191f3a1ef3e0b3f5c65f6c9f8fcc0e68707ae
TheTeaCup <lw0482352@gmail.com>
2026-04-05 18:15:21 -0400

    wip

---
+++ src/utils/appalcartUpdates.ts
@@ -0,0 +1,36 @@
+import { WebhookClient } from "discord.js";
+import { config } from "../config.js";
+
+const webhook = new WebhookClient({
+  url: config.,
+});
+const APPALCART_UPDATES = "https://appalcart.etaspot.net/service.php?service=get_service_announcements&token=appstate";
+
+async function fetchAppalcartUpdates() {
+  try {
+    const response = await fetch(APPALCART_UPDATES);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    if (data.get_service_announcements) {
+        if (data.get_service_announcements.length === 0) {
+            console.log("No service announcements found.");
+            return [];
+        }
+
+        if (data.get_service_announcements.length > 0) {
+            console.log("Service announcements found:");
+            data.get_service_announcements.forEach((announcement: any, index: number) => {
+                console.log(`Announcement ${index + 1}:`, announcement);
+            });
+        }
+    }
+    // return data.announcements || [];
+  } catch (error) {
+    console.error("Failed to fetch Appalcart updates:", error);
+    return [];
+  }
+}
+
+fetchAppalcartUpdates();
